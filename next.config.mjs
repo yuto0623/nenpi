331172082098f07a6ff4f4b7cbbfeffdfdcafdf4
yuto0623 <yuto.ryr0623@gmail.com>
2024-05-03 14:39:29 +0900

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+import withPWA from "next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+const withPWAConfig = withPWA({
+  disable: process.env.NODE_ENV === 'development',
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+export default withPWAConfig(nextConfig)
