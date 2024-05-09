77a03f3038fc35d0e5358c62bf58faa7e22b65e0
yuto0623 <yuto.ryr0623@gmail.com>
2024-05-09 13:35:45 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,13 @@ import { Spinner } from "@nextui-org/react";
 
 export default function Home() {
 	const { data: session, status } = useSession();
-	const [user, setUser] = useState("");
+	const [user, setUser] = useState({
+		id: "",
+		name: "",
+		email: "",
+		image: "",
+		mileage: null,
+	});
 
 	const getUserDetail = async () => {
 		// const body = {
@@ -22,26 +28,37 @@ export default function Home() {
 			params: { id },
 			// headers: { "Content-Type": "application/json" },
 		});
-		console.log(response.data);
-		setUser(response.data.mileage);
+		// console.log(response.data);
+		setUser(response.data);
 	};
 
 	useEffect(() => {
-		getUserDetail();
-	}, [getUserDetail]);
+		if (!user.mileage) {
+			getUserDetail();
+		}
+	}, [getUserDetail, user]);
 
-	const onSubmit = async () => {};
+	const onSubmit = async (formData: FormData) => {
+		if (!user) return;
+		const putBody = {
+			id: user.id,
+			mileage: formData.get("mileage"),
+		};
+		const response = await axios.put("/api/user", JSON.stringify(putBody));
+		// console.log(response.data);
+		getUserDetail();
+	};
 
 	return (
 		<main>
 			{status === "authenticated" ? (
 				<>
 					<Navigationbar />
-					<div>
-						<Input />
-						<Button onClick={() => onSubmit()}>送信</Button>
-						{user ? <p>{user}</p> : <Spinner />}
-					</div>
+					<form action={onSubmit}>
+						<Input name="mileage" />
+						<Button type="submit">送信</Button>
+						{user ? <p>{user.mileage}</p> : <Spinner />}
+					</form>
 				</>
 			) : (
 				<Login />
