b42893b92d547fe202781d10fc57a51535be8c3f
yuto0623 <yuto.ryr0623@gmail.com>
2024-05-11 15:35:28 +0900

    update

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -9,41 +9,41 @@ export async function GET(req: NextRequest, res: NextResponse) {
   try {
     const id = await req.nextUrl.searchParams.get("id");
     if (!id) {
-     return new Response(JSON.stringify({ error: "ID is required" }), { status: 400 });
+      return new Response(JSON.stringify({ error: "ID is required" }), { status: 400 });
     }
     const user = await prisma.user.findUnique(
-      {where: {id: id}}
+      { where: { id: id } }
     );
     if (!user) {
-     return new Response(JSON.stringify({ error: "User not found" }), { status: 404 });
+      return new Response(JSON.stringify({ error: "User not found" }), { status: 404 });
     }
-    
+
     return new Response(JSON.stringify(user));
   } catch (e) {
-   console.error(e);
-   return new Response(JSON.stringify({ error: "Internal Server Error" }), { status: 500 });
- } finally {
-   await prisma.$disconnect();
- }
-}
-
-export async function PUT(req: NextRequest, res: NextResponse) {
-  const prisma = new PrismaClient();
-  try {
-  const body = await req.json()
-
-  await prisma.user.update({
-    where: {
-      id: body.id
-    },
-    data: {
-      mileage: Number(body.mileage)
-    }
-  })
-  return new Response(JSON.stringify(body));
-  } catch (e) {
+    console.error(e);
     return new Response(JSON.stringify({ error: "Internal Server Error" }), { status: 500 });
-  }finally {
-   await prisma.$disconnect();
- }
+  } finally {
+    await prisma.$disconnect();
+  }
 }
+
+// export async function PUT(req: NextRequest, res: NextResponse) {
+//   const prisma = new PrismaClient();
+//   try {
+//   const body = await req.json()
+
+//   await prisma.user.update({
+//     where: {
+//       id: body.id
+//     },
+//     data: {
+//       mileage: Number(body.mileage)
+//     }
+//   })
+//   return new Response(JSON.stringify(body));
+//   } catch (e) {
+//     return new Response(JSON.stringify({ error: "Internal Server Error" }), { status: 500 });
+//   }finally {
+//    await prisma.$disconnect();
+//  }
+// }
