d9b63349fbcadea76a10f1bfea8ebc48c6b509f6
yuto0623 <yuto.ryr0623@gmail.com>
2024-05-13 16:02:14 +0900

    update

---
+++ app/api/user/[id]/route.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+import type { NextRequest, NextResponse } from "next/server";
+
+
+
+export async function GET(req: NextRequest, res: NextResponse,
+  { params }: { params: { id: string } }
+) {
+  const prisma = new PrismaClient();
+  // const body = await req.json();
+  try {
+    // const id = await req.nextUrl.searchParams.get("id");
+    const id = await params.id;
+    if (!id) {
+      return new Response(JSON.stringify({ error: "ID is required" }), { status: 400 });
+    }
+    const user = await prisma.user.findUnique(
+      { where: { id: id } }
+    );
+    if (!user) {
+      return new Response(JSON.stringify({ error: "User not found" }), { status: 404 });
+    }
+
+    return new Response(JSON.stringify(user));
+  } catch (e) {
+    console.error(e);
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,52 +1,52 @@
-import { PrismaClient } from "@prisma/client";
-import type { NextRequest, NextResponse } from "next/server";
+// import { PrismaClient } from "@prisma/client";
+// import type { NextRequest, NextResponse } from "next/server";
 
 
 
-export async function GET(req: NextRequest, res: NextResponse,
-  { params }: { params: { id: string } }
-) {
-  const prisma = new PrismaClient();
-  // const body = await req.json();
-  try {
-    // const id = await req.nextUrl.searchParams.get("id");
-    const id = await params.id;
-    if (!id) {
-      return new Response(JSON.stringify({ error: "ID is required" }), { status: 400 });
-    }
-    const user = await prisma.user.findUnique(
-      { where: { id: id } }
-    );
-    if (!user) {
-      return new Response(JSON.stringify({ error: "User not found" }), { status: 404 });
-    }
-
-    return new Response(JSON.stringify(user));
-  } catch (e) {
-    console.error(e);
-    return new Response(JSON.stringify({ error: "Internal Server Error" }), { status: 500 });
-  } finally {
-    await prisma.$disconnect();
-  }
-}
-
-// export async function PUT(req: NextRequest, res: NextResponse) {
+// export async function GET(req: NextRequest, res: NextResponse,
+//   { params }: { params: { id: string } }
+// ) {
 //   const prisma = new PrismaClient();
+//   // const body = await req.json();
 //   try {
-//   const body = await req.json()
-
-//   await prisma.user.update({
-//     where: {
-//       id: body.id
-//     },
-//     data: {
-//       mileage: Number(body.mileage)
+//     // const id = await req.nextUrl.searchParams.get("id");
+//     const id = await params.id;
+//     if (!id) {
+//       return new Response(JSON.stringify({ error: "ID is required" }), { status: 400 });
 //     }
-//   })
-//   return new Response(JSON.stringify(body));
+//     const user = await prisma.user.findUnique(
+//       { where: { id: id } }
+//     );
+//     if (!user) {
+//       return new Response(JSON.stringify({ error: "User not found" }), { status: 404 });
+//     }
+
+//     return new Response(JSON.stringify(user));
 //   } catch (e) {
+//     console.error(e);
 //     return new Response(JSON.stringify({ error: "Internal Server Error" }), { status: 500 });
-//   }finally {
-//    await prisma.$disconnect();
-//  }
+//   } finally {
+//     await prisma.$disconnect();
+//   }
 // }
+
+// // export async function PUT(req: NextRequest, res: NextResponse) {
+// //   const prisma = new PrismaClient();
+// //   try {
+// //   const body = await req.json()
+
+// //   await prisma.user.update({
+// //     where: {
+// //       id: body.id
+// //     },
+// //     data: {
+// //       mileage: Number(body.mileage)
+// //     }
+// //   })
+// //   return new Response(JSON.stringify(body));
+// //   } catch (e) {
+// //     return new Response(JSON.stringify({ error: "Internal Server Error" }), { status: 500 });
+// //   }finally {
+// //    await prisma.$disconnect();
+// //  }
+// // }
