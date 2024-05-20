97f5110373de9f961490f7d76efb13bce4bb78f8
yuto0623 <yuto.ryr0623@gmail.com>
2024-05-20 14:57:46 +0900

    update

--- components/History/Chart/Chart.tsx
+++ components/History/Chart/Chart.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody } from "@nextui-org/react";
+import { Card, CardBody, Tabs, Tab } from "@nextui-org/react";
 import type { DataList } from "@prisma/client";
 import React from "react";
 import {
@@ -22,7 +22,9 @@ export default function Chart(dataList: { dataList: DataList[] }) {
 			hour: "numeric",
 		}),
 		mileage: item.mileage,
+		給油までの走行距離: item.mileageIncrement,
 		ガソリン価格: item.gasPrice,
+		給油量: item.gas,
 	}));
 
 	const reversedData = transformedData.reverse();
@@ -30,28 +32,95 @@ export default function Chart(dataList: { dataList: DataList[] }) {
 	return (
 		<Card>
 			<CardBody>
-				<ResponsiveContainer width="100%" height={300}>
-					<LineChart
-						width={700}
-						height={300}
-						data={reversedData}
-						margin={{
-							top: 5,
-							right: 5,
-							left: 5,
-							bottom: 5,
-						}}
-					>
-						<CartesianGrid strokeDasharray="3 3" />
-						<XAxis dataKey="name" />
-						<YAxis />
-						<Line type="monotone" dataKey="ガソリン価格" stroke="#8884d8" />
-						{/* <Line type="monotone" dataKey="正解数" stroke="#3ba2f6" />
+				<Tabs aria-label="History">
+					<Tab key="gasPrice" title="ガソリン価格">
+						<ResponsiveContainer width="100%" height={300}>
+							<LineChart
+								width={700}
+								height={300}
+								data={reversedData}
+								margin={{
+									top: 5,
+									right: 5,
+									left: 5,
+									bottom: 5,
+								}}
+							>
+								<CartesianGrid strokeDasharray="3 3" />
+								<XAxis dataKey="name" />
+								<YAxis />
+								<Line
+									type="monotone"
+									dataKey="ガソリン価格"
+									stroke="#8884d8"
+									unit="円"
+								/>
+								{/* <Line type="monotone" dataKey="正解数" stroke="#3ba2f6" />
 					<Line type="monotone" dataKey="正解率" stroke="#ff0092" /> */}
-						<Legend />
-						<Tooltip />
-					</LineChart>
-				</ResponsiveContainer>
+								<Legend />
+								<Tooltip />
+							</LineChart>
+						</ResponsiveContainer>
+					</Tab>
+					<Tab key="mileageIncrement" title="給油までの走行距離">
+						<ResponsiveContainer width="100%" height={300}>
+							<LineChart
+								width={700}
+								height={300}
+								data={reversedData}
+								margin={{
+									top: 5,
+									right: 5,
+									left: 5,
+									bottom: 5,
+								}}
+							>
+								<CartesianGrid strokeDasharray="3 3" />
+								<XAxis dataKey="name" />
+								<YAxis />
+								<Line
+									type="monotone"
+									dataKey="給油までの走行距離"
+									stroke="#8884d8"
+									unit="Km"
+								/>
+								{/* <Line type="monotone" dataKey="正解数" stroke="#3ba2f6" />
+					<Line type="monotone" dataKey="正解率" stroke="#ff0092" /> */}
+								<Legend />
+								<Tooltip />
+							</LineChart>
+						</ResponsiveContainer>
+					</Tab>
+					<Tab key="gas" title="給油量">
+						<ResponsiveContainer width="100%" height={300}>
+							<LineChart
+								width={700}
+								height={300}
+								data={reversedData}
+								margin={{
+									top: 5,
+									right: 5,
+									left: 5,
+									bottom: 5,
+								}}
+							>
+								<CartesianGrid strokeDasharray="3 3" />
+								<XAxis dataKey="name" />
+								<YAxis />
+								<Line
+									type="monotone"
+									dataKey="給油量"
+									stroke="#8884d8"
+									unit="L"
+								/>
+								{/* <Line type="monotone" dataKey="正解数" stroke="#3ba2f6" />
+					<Line type="monotone" dataKey="正解率" stroke="#ff0092" /> */}
+								<Legend />
+								<Tooltip />
+							</LineChart>
+						</ResponsiveContainer>
+					</Tab>
+				</Tabs>
 			</CardBody>
 		</Card>
 	);
